3797280f4e7717c5dca2f0f2c88c30cd461384f2
Laurence Liang <laurence.w.liang@gmail.com>
2025-02-27 20:18:46 -0500

    Create placeholder train files

---
+++ scripts/train_ffnn_simple.py
@@ -0,0 +1,7 @@
+from sklearn.neural_network import MLPRegressor
+
+def train_ffnn_simple():
+    pass
+
+if __name__ == "__main__":
+    train_ffnn_simple()
---
+++ scripts/train_log_reg.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LogisticRegression
+
+def train_logistic_regression(pkl_filepath=""):
+    # read data
+    # fit
+    # export to .pkl file
+
+if __name__ == "__main__":
+    train_logistic_regression()
